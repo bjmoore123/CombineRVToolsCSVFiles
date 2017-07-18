214d048735a1ee20c07b0b84569d78fbf4e1c9c6
bjmoore123 <moore.ben@gmail.com>
2017-07-18 11:55:19 -0400

    Update combinefiles.ps1

--- combinefiles.ps1
+++ combinefiles.ps1
@@ -1,3 +1,9 @@
+#CombineRVToolsCSVReports.ps1
+#Ben Moore
+#7/18/17
+#Used to prep RVTools reports for import to dashboard
+
+#build array of report types
 $reportTypes = @(
 	"tabvInfo";
 	"tabdvPort";
@@ -29,10 +35,13 @@ foreach($reportType in $reportTypes) {
     Clear-Variable consolidatedReportFile -Scope Global
     Clear-Variable reportFiles -Scope Global
 
+    #built consolidated report file name
     $consolidatedReportFile = "$reportType$(get-date -f yyyy-MM-dd_hh_mm).csv"
 	
+    #identify reports of same type in running directory
     $reportFiles = Get-ChildItem -filter *$reportType*
     foreach($reportFile in $reportFiles) {
+        #combine reports and drop header line from each file
         (Get-Content $reportFile)[1 .. $reportFile.Length] | Add-Content $consolidatedReportFile
     }
 }
