4040a36d4def463cd9023fceb9f0d29e5e866cfa
bjmoore123 <moore.ben@gmail.com>
2017-07-18 11:49:07 -0400

    combinefiles.ps1

---
+++ combinefiles.ps1
@@ -0,0 +1,38 @@
+$reportTypes = @(
+	"tabvInfo";
+	"tabdvPort";
+	"tabdvSwitch";
+	"tabvCD";
+	"tabvCluster";
+	"tabvCPU";
+	"tabvDatastore";
+	"tabvDisk";
+	"tabvFloppy";
+	"tabvHBA";
+	"tabvHealth";
+	"tabvHost";
+	"tabvMemory";
+	"tabvMultiPath";
+	"tabvNetwork";
+	"tabvNIC";
+	"tabvPartition";
+	"tabvPort";
+	"tabvRP";
+	"tabvSC_VMK";
+	"tabvSnapshot";
+	"tabvSwitch";
+	"tabvTools";
+)
+
+foreach($reportType in $reportTypes) {
+
+    Clear-Variable consolidatedReportFile -Scope Global
+    Clear-Variable reportFiles -Scope Global
+
+  	$consolidatedReportFile = "$reportType$(get-date -f yyyy-MM-dd_hh_mm).csv"
+	
+    $reportFiles = Get-ChildItem -filter *$reportType*
+    foreach($reportFile in $reportFiles) {
+        (Get-Content $reportFile)[1 .. $reportFile.Length] | Add-Content $consolidatedReportFile
+    }
+}
